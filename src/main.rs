bf7174f7237d3c37b01ffaeb81e224b410377c24
Andrei Sonea <35298550+andreiverse@users.noreply.github.com>
2026-03-26 21:54:37 +0200

    fix deps

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ mod dto;
 mod entities;
 mod service;
 
-use axum::Router;
+use axum::{
+    Router, error_handling::HandleErrorLayer, http::Uri, response::IntoResponse, routing::get,
+};
 use sea_orm::{Database, DatabaseConnection};
-use tracing::Level;
+use utoipa_axum::router::OpenApiRouter;
 use std::net::SocketAddr;
+use tower::ServiceBuilder;
 use tower_http::cors::CorsLayer;
 use tower_sessions::{
     Expiry, MemoryStore, SessionManagerLayer,
     cookie::{SameSite, time::Duration},
 };
+use tracing::Level;
 use utoipa_swagger_ui::SwaggerUi;
+
+use axum_oidc::{
+    EmptyAdditionalClaims, OidcAuthLayer, OidcClaims, OidcClient, OidcLoginLayer,
+    OidcRpInitiatedLogout,
+    error::MiddlewareError,
+    handle_oidc_redirect,
+    openidconnect::{Audience, ClientId, ClientSecret, IssuerUrl, Scope},
+};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -41,12 +53,17 @@ async fn main() -> Result<(), anyhow::Error> {
         .with_secure(false)
         .with_same_site(SameSite::Lax)
         .with_expiry(Expiry::OnInactivity(Duration::seconds(120)));
+    let api_router = controller::create_controller();
 
-    let app = Router::new()
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api))
-        .merge(router)
-        .layer(cors)
+    let full_router = OpenApiRouter::new()
+        .merge(api_router) // merge while still OpenApiRouter
         .layer(session_layer)
+        .layer(cors);
+
+    let (router, api) = full_router.split_for_parts();
+
+    let app = router
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", api))
         .with_state(state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -55,4 +72,25 @@ async fn main() -> Result<(), anyhow::Error> {
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
+}
+
+async fn authenticated(claims: OidcClaims<EmptyAdditionalClaims>) -> impl IntoResponse {
+    format!("Hello {}", claims.subject().as_str())
+}
+
+async fn maybe_authenticated(
+    claims: Result<OidcClaims<EmptyAdditionalClaims>, axum_oidc::error::ExtractorError>,
+) -> impl IntoResponse {
+    if let Ok(claims) = claims {
+        format!(
+            "Hello {}! You are already logged in from another Handler.",
+            claims.subject().as_str()
+        )
+    } else {
+        "Hello anon!".to_string()
+    }
+}
+
+async fn logout(logout: OidcRpInitiatedLogout) -> impl IntoResponse {
+    logout.with_post_logout_redirect(Uri::from_static("https://example.com"))
 }
